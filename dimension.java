76d89d45c67c2387cbbd7cc292eb30937839fef8
preethisaisankaran <preethisankaran11@gmail.com>
2023-03-30 16:41:47 +0530

    Added:link

--- dimension.java
+++ dimension.java
@@ -1,2 +1,15 @@
-package ArrayProblems;public class dimension {
-}
+//package ArrayProblems;
+
+//public class dimension {
+    //public static void main(String[] args) //{
+
+
+        //int[][][] arr3 = new int[3][4][3];
+
+       // for (int i = 0; i < arr3.length; i++) {
+         //   for (int j = 0; j < arr3[i].length; j++) {
+           //     for (int k = 0; k < arr3[i][j].length; k++) {
+             //       System.out.println(arr3[i][j][k]);
+               // }
+            //}
+        //}
