41d764f6c7650ec8ae4fe754c45f48178b36e426
preethisaisankaran <preethisankaran11@gmail.com>
2023-03-30 15:56:32 +0530

    Added:code to print odd position num

--- oddPosition.java
+++ oddPosition.java
@@ -1,2 +1,10 @@
-package ArrayProblems;public class oddPosition {
+package ArrayProblems;
+
+public class oddPosition {
+    public static void main(String[] args) {
+        int[] odd = {23, 45, 78, 90, 33, 42, 77};
+        for (int i = 0; i < odd.length; i = i + 2) {
+            System.out.println("odd position elements are:" + odd[i]);
+        }
+    }
 }
